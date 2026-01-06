1862b993796aa9126ba0dbe35fb4b77f79b0310f
Vityy <ygensoftvd@gmail.com>
2026-01-06 16:59:56 +0100

    Components created

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('game-catalog-angular');
+  protected readonly title = signal('Dashboard');
 }
